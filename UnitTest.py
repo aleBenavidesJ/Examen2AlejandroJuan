011f8b6ea642b9192211b09960cfbf8f02ce056f
Juan Rodriguez <juanrm071299@gmail.com>
2023-11-12 22:19:08 -0600

    Unit tests for ObtieneMasBailable and VerificarListaCanciones methods

--- UnitTest.py
+++ UnitTest.py
@@ -22,6 +22,22 @@ class UnitTest(unittest.TestCase):
         resultado = self.objeto.DivisibleTempo(0)
         self.assertEqual(resultado, [])
 
+    def testObtieneMasBailable1(self):
+        resultado = self.objeto.ObtieneMasBailable([0.8, 0.9, 0.7])
+        self.assertEqual(resultado, 0.9)
+
+    def testObtieneMasBailable2(self):
+        resultado = self.objeto.ObtieneMasBailable([])
+        self.assertIsNone(resultado)
+
+    def testVerificaListaCanciones1(self):
+        resultado = self.objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"])
+        self.assertTrue(resultado)
+
+    def testVerificaListaCanciones2(self):
+        resultado = self.objeto.VerificaListaCanciones(["Canción 1", "Canción 2", None])
+        self.assertFalse(resultado)
+
 
 if __name__ == '__main__':
     unittest.main()
